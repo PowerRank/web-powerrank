7d4cf41acf07bb90449843096c46e5318cc1aa60
Omar Ahmed <omarahmed1121@gmail.com>
2023-10-22 17:50:20 -0500

    add header

---
+++ app/components/header.tsx
@@ -0,0 +1,25 @@
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import logo from '~/assets/images/poro-logo.png'
+
+export default function Header() {
+
+    return (
+        <Navbar expand="sm" className="navbar-dark p-3">
+                <Navbar.Brand href="/">
+                    <img
+                        src={logo}
+                        width="40"
+                        height="40"
+                        alt="PoroRank"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="justify-content-start flex-grow-1">
+                        <Nav.Link href="/world-ranking">World Ranking</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
+    );
+}
