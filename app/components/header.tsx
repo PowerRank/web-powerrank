98c1049a6b55d2dfe2a24cf94638ab10d4c3f9e0
Omar Ahmed <omarahmed1121@gmail.com>
2023-10-23 15:05:48 -0500

    cleanup header brand

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,20 +6,21 @@ export default function Header() {
 
     return (
         <Navbar expand="sm" className="navbar-dark p-3">
-                <Navbar.Brand href="/">
-                    <img
-                        src={logo}
-                        width="40"
-                        height="40"
-                        alt="PoroRank"
-                    />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="justify-content-start flex-grow-1">
-                        <Nav.Link href="/world-ranking">World Ranking</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Navbar.Brand href="/">
+                <img
+                    src={logo}
+                    width="40"
+                    height="40"
+                    alt="PoroRank"
+                />{' '}
+                PoroRank
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="justify-content-start flex-grow-1">
+                    <Nav.Link href="/world-ranking">World Ranking</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 }
